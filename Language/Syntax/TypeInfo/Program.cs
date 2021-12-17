c00f65bddef8b74d13bf4bfebeb5ae49ef34aa42
Taylor Huang <taylorw@126.com>
2021-12-17 10:43:09 +0800

    How to get the type information of an object

---
+++ Language/Syntax/TypeInfo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TypeInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 1, 2,3,4,5};
+            var type = numbers.GetType();
+            Console.WriteLine(type.FullName);
+        }
+    }
+}
