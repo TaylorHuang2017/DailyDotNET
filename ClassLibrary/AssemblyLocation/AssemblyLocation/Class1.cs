ec23a41248190239a475989dbf9da6956e3139c6
Taylor Huang <taylorw@126.com>
2022-03-19 15:52:47 +0800

    add wpf

---
+++ ClassLibrary/AssemblyLocation/AssemblyLocation/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace AssemblyLocation
+{
+    public class Class1
+    {
+        public void Test()
+        {
+            Console.WriteLine(typeof(FileStream).Assembly.Location);
+        }
+    }
+}
--- Language/Syntax/StartApp/obj/Debug/net5.0-windows/App.g.cs
+++ Language/Syntax/StartApp/obj/Debug/net5.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace StartApp {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.15.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace StartApp {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.15.0")]
         public static void Main() {
             StartApp.App app = new StartApp.App();
             app.InitializeComponent();
